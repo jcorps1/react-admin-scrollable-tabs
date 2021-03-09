64c5236f2f8ebd998b817f0cce6c8fa7e66c2380
Josh Corps <jcorps@ascenti.co.uk>
2021-03-09 23:26:16 +0000

    Initial commit

---
+++ src/posts.js
@@ -0,0 +1,99 @@
+import * as React from 'react';
+import {
+    Show,
+    ShowButton,
+    SimpleShowLayout,
+    RichTextField,
+    DateField,
+    List,
+    Edit,
+    Create,
+    Datagrid,
+    ReferenceField,
+    TextField,
+    EditButton,
+    ReferenceInput,
+    SelectInput,
+    SimpleForm,
+    TextInput,
+    Filter,
+    TabbedShowLayout,
+    Tab,
+    TabbedShowLayoutTabs,
+} from 'react-admin';
+
+const PostFilter = props => (
+    <Filter {...props}>
+        <TextInput label="Search" source="q" alwaysOn />
+        <ReferenceInput
+            label="User"
+            source="userId"
+            reference="users"
+            allowEmpty
+        >
+            <SelectInput optionText="name" />
+        </ReferenceInput>
+    </Filter>
+);
+
+export const PostList = props => (
+    <List {...props} filters={<PostFilter />}>
+        <Datagrid>
+            <TextField source="id" />
+            <ReferenceField label="User" source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
+            <TextField source="title" />
+            <EditButton />
+            <ShowButton />
+        </Datagrid>
+    </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
+export const PostEdit = props => (
+    <Edit title={<PostTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <ReferenceInput label="User" source="userId" reference="users">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+            <TextInput source="title" />
+            <TextInput multiline source="body" />
+        </SimpleForm>
+    </Create>
+);
+
+export const PostShow = props => (
+    <Show {...props}>
+        <TabbedShowLayout tabs={<TabbedShowLayoutTabs variant="scrollable" {...props} />} >
+            <Tab label="Summary">
+                <TextField source="title" />
+                <TextField source="teaser" />
+                <RichTextField source="body" />
+                <DateField label="Publication date" source="created_at" />
+            </Tab>
+            <Tab label="Summary"></Tab>
+            <Tab label="Summary"></Tab>
+            <Tab label="Summary"></Tab>
+            <Tab label="Summary"></Tab>
+            <Tab label="Summary"></Tab>
+            <Tab label="Summary"></Tab>
+        </TabbedShowLayout>
+    </Show>
+);
